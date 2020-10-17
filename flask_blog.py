9692cbf72c1aefd5bd4dd0e1feed5787df286e3a
Prakash3008 <prak30ash@gmail.com>
2020-10-18 01:10:04 +0530

    base home page

--- flask_blog.py
+++ flask_blog.py
@@ -1,16 +1,30 @@
-from flask import Flask, escape, request,render_template
+from flask import Flask, escape, request,render_template, url_for
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Prakash',
+        'title': 'Post 1',
+        'content': 'First Post',
+        'date_posted': 'April 10, 2020'
+    },
+    {
+        'author': 'Ash',
+        'title': 'Post 2',
+        'content': 'Second Post',
+        'date_posted': 'April 29, 2020'
+    }
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    name = request.args.get("name", "User, this is about page")
-    return f'<h1>Hello, {escape(name)}!</h1>'
+    return render_template('about.html', title='About title')
 
 if __name__ == '__main__':
     app.run(debug=True)
